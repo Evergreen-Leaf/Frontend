095215326fc972d02d85d44dabf469069001f56f
rumians <ruanfort12@gmail.com>
2025-06-13 13:46:37 -0300

    FEAT: criando a tela de cadastro

--- src/components/cadastro/cadastrocomp.vue
+++ src/components/cadastro/cadastrocomp.vue
@@ -1,14 +1,32 @@
+<script setup>
+import titlecadastro from '@/components/cadastro/titlecadastro.vue';
+</script>
 <template>
-    <div class="container">
-        cadastro
+    <div class="background">
+        <div class="container">
+            <titlecadastro />
+        </div>
     </div>
 </template>
 <style scoped>
-* {
+.background {
   background-image: url('../../../public/background.png');
   background-repeat: no-repeat;
   background-size: cover;
   font-family: Arial, sans-serif;
   height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 85%;
+  width: 40%;
+  background-color: white;
+  border-radius: 10px;
 }
 </style>
---
+++ src/components/cadastro/titlecadastro.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div>
+            <h1>CADASTRO</h1>
+        </div>
+        <div>
+            <p>Preencha suas informações</p>
+        </div>
+    </div>
+</template>
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 20%;
+    width: 100%;
+}
+h1 {
+    font-size: 40px;
+    color: #28434C;
+    margin-bottom: 5px;
+}
+p {
+    font-size: 20px;
+    color: #28434C;
+    margin-top: 0;
+}
+</style>
