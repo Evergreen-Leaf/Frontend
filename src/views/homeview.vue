c23a62c89fa2eeedf0b69a3c8cd53b0e938e4a56
rumians <ruanfort12@gmail.com>
2025-08-12 15:02:36 -0300

    FEAT: Conectando front com o back pelo axios

--- src/views/homeview.vue
+++ src/views/homeview.vue
@@ -1,6 +1,11 @@
 <script setup>
+import { useCarrinhoStore } from '@/stores/teste';
 import headercomp from '@/components/header/headercomp.vue';
+
+const teste = useCarrinhoStore()
+const lista = teste.getCarrinho()
 </script>
 <template>
 <headercomp />
+<p>{{ lista }}</p>
 </template>
