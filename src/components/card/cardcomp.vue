152fdbe0365e4ec4708e006a9bbe031a0ca1aaed
rumians <ruanfort12@gmail.com>
2025-08-21 16:04:52 -0300

    FEAT: Criaçao do card de produto

---
+++ src/components/card/cardcomp.vue
@@ -0,0 +1,45 @@
+<script setup>
+import cardimage from './cardimage.vue';
+import cardinfo from './cardinfo.vue';
+</script>
+<template>
+    <div class="container">
+        <div class="card-container">
+                <cardimage />
+                <cardinfo />
+        </div>
+        <div class="card-container">
+                <cardimage />
+                <cardinfo />
+        </div>
+        <div class="card-container">
+                <cardimage />
+                <cardinfo />
+        </div>
+        <div class="card-container">
+                <cardimage />
+                <cardinfo />
+        </div>
+    </div>
+</template>
+<style scoped>
+.container {
+    display: flex;
+    background-color: #EEEEEE;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-wrap: wrap;
+    width: 100%;
+    height: 50vh;
+}
+
+.card-container {
+    width: 20%;
+    height: 95%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-shadow: 0px 20px 20px rgba(0, 0, 0, 0.25);
+    border-radius: 12px;
+}
+</style>
---
+++ src/components/card/cardinfo.vue
@@ -0,0 +1,39 @@
+<script setup>
+</script>
+<template>
+<div class="container-info">
+    <p class="title">Chips de Arroz e Milho</p>
+    <p class="description">Descriçaõ do produtoDescriçaõ do produtoDescriçaõ do produto</p>
+    <p class="discount">R$ 24,90</p>
+    <p class="price">R$ 20,90</p>
+</div>
+</template>
+<style scoped>
+.container-info {
+    display: grid;
+    grid-template-rows: 1fr 1fr 1fr 0.5fr 0.6fr;
+    width: 90%;
+    height: 40%;
+}
+
+.title {
+    margin-top: 1rem;
+    font-size: 18px;
+    font-weight: bold;
+}
+.description {
+    font-size: 15px;
+    color: rgba(0, 0, 0, 0.56);   
+}
+.discount {
+    font-size: 14px;
+    text-decoration: line-through;
+    color: rgba(0, 0, 0, 0.29);
+     grid-row: 4/5;
+}
+.price {
+    font-size: 18px;
+    font-weight: bold;
+    grid-row: 5/5;
+}
+</style>
