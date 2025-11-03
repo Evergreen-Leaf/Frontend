a539648e524ec015ee1cfda44e0789e21915ea5a
rumians <ruanfort12@gmail.com>
2025-11-03 09:26:19 -0300

    FEAT: arrumandoo

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.t0he7j0frjo"
+    "revision": "0.3h2d5vm22sg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
