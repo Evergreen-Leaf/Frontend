3642cfe0c3db2b90854a473cceb522c921b75195
rumians <ruanfort12@gmail.com>
2025-11-14 16:09:54 -0300

    fix: update axios baseURL to use environment variable

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useUsuarioStore } from '@/stores/usuario'
 
 const api = axios.create({
-  baseURL: 'http://localhost:19003/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:19003/api',
 })
 
 api.interceptors.request.use((config => {
