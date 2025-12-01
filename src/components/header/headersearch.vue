ff8b4cdb8ab99e758a35eac90fd8526275fab8da
ohdavir <davi.rosa.info@gmail.com>
2025-12-01 13:47:22 -0300

    feat: melhorias na responsividade e ajustes de estilo em componentes do cabeçalho e home

--- src/components/header/headersearch.vue
+++ src/components/header/headersearch.vue
@@ -7,32 +7,40 @@ defineProps({
 <template>
   <div class="container">
     <div class="container-pesquisa">
-      <div class="pesquisa" :style="{ borderColor: scrolled ? 'rgba(0, 0, 0, 0.253)' : 'rgba(255, 255, 255, 0.493)' }">
+      <div 
+        class="pesquisa" 
+        :class="{ scrolled }"
+        :style="{ borderColor: scrolled ? 'rgba(0, 0, 0, 0.253)' : 'rgba(255, 255, 255, 0.493)' }">
+        
         <input
           type="search"
           placeholder="Buscar por"
           :class="{ scrolled }"
         />
+
         <img v-if="!scrolled" src="/public/search-icon.png" />
         <img v-else src="/public/search-icon-black.png" />
       </div>
     </div>
   </div>
-</template> 
+</template>
 
 <style scoped>
 .container {
   position: fixed;
   top: 5%;
-  left: 60%;
-  width: 60vw;
+  left: 50%;
   transform: translate(-50%, -50%);
+  width: 40%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .container-pesquisa {
   display: flex;
-  justify-content: space-evenly;
-  width: 70%;
+  justify-content: center;
+  width: 100%;
   background-color: transparent;
   border-radius: 2rem;
 }
@@ -42,11 +50,16 @@ defineProps({
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 100%;
   border: 1.5px solid rgba(0, 0, 0, 0.253);
   padding: 0.6rem;
   border-radius: 2rem;
 }
+
+/* Quando scrolled = true */
+.pesquisa.scrolled {
+  border: 1px solid rgba(0, 0, 0, 0.253);
+}
+
 input {
   border: none;
   outline: none;
@@ -56,17 +69,14 @@ input {
   color: white;
   transition: color 0.3s, background-color 0.3s;
 }
-.pesquisa.scrolled {
-    border: 1px solid rgba(0, 0, 0, 0.253);
-}
+
 input::placeholder {
   color: white;
-  transition: color 0.3s;
 }
 
+/* Input quando scrolled = true */
 input.scrolled {
   color: black;
-
 }
 
 input.scrolled::placeholder {
